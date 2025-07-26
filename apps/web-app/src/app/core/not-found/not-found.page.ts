35d9f51bd11da1df8fab4983f026c1e28fce7f05
NoNamer777 <oscar.wellner@gmail.com>
2025-07-26 23:47:49 +0200

    Add NotFound page

---
+++ apps/web-app/src/app/core/not-found/not-found.page.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import { NotFoundIconComponent } from '../../shared';
+
+@Component({
+    selector: 'cml-not-found',
+    templateUrl: './not-found.page.html',
+    styleUrl: './not-found.page.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [NotFoundIconComponent],
+})
+export class NotFoundPage {
+    private readonly router = inject(Router);
+    private readonly destroyRef = inject(DestroyRef);
+
+    protected onVisitHomePage() {
+        from(this.router.navigateByUrl('/')).pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+    }
+}
