dde55452e2caddd4989bedbbdabd4654ef2da894
Menno Wellner <menno.wellner@hotmail.com>
2025-07-26 19:05:40 +0200

    Adds repository interface for Item model and faking it with an in-memory database from CSV

---
+++ apps/backend/src/main/kotlin/model/FakeItemRepository.kt
@@ -0,0 +1,28 @@
+package org.eu.nl.model
+
+import io.netty.util.internal.ResourcesUtil
+
+class FakeItemRepository : ItemRepository {
+    private val items = readCsv()
+
+    fun readCsv(): MutableList<Item> {
+        return this.javaClass.classLoader.getResourceAsStream("Medieval_Dynasty_Items.csv").reader().readLines()
+            .filter { it.isNotBlank() }
+            .map {
+                val (name, weight, baseValue) = it.split(',', ignoreCase = false, limit = 3)
+                Item(name = name, weight = weight.trim().toDouble(), baseValue = baseValue.trim().toDouble())
+            }.toMutableList()
+    }
+
+    override fun allItems(): List<Item> = items
+
+    override fun itemByName(name: String) = items.find {
+        it.name.equals(name, ignoreCase = true)
+    }
+
+    override fun addItem(item: Item) {
+        if (itemByName(item.name) != null) throw IllegalArgumentException("Item already exists")
+    }
+
+    override fun removeItem(name: String): Boolean = items.removeIf { it.name == name }
+}
