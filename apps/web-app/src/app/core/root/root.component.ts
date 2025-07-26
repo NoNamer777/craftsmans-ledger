1cefd4a9941b85529031218a4c0d2942e6bc9a23
NoNamer777 <oscar.wellner@gmail.com>
2025-07-26 23:47:06 +0200

    Add HeaderComponent with basic routing

--- apps/web-app/src/app/core/root/root.component.ts
+++ apps/web-app/src/app/core/root/root.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header';
 
 @Component({
     selector: 'cml-root',
     templateUrl: './root.component.html',
     styleUrl: './root.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RouterModule],
+    imports: [RouterModule, HeaderComponent],
 })
 export class RootComponent {}
