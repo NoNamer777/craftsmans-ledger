457a82dfa7c43e27a10561007140e42fcdd36376
NoNamer777 <oscar.wellner@gmail.com>
2025-07-27 16:53:33 +0200

    Add ApiService to make requests to the API backend

--- apps/web-app/src/app/shared/http/models.ts
+++ apps/web-app/src/app/shared/http/models.ts
@@ -1,3 +1,10 @@
 export type QueryParamType = string | number | boolean;
 
 export type QueryParams = Record<string, QueryParamType | QueryParamType[]>;
+
+export interface PaginatedResponse<T> {
+    data: T[];
+    maxPages: number;
+    page: number;
+    totalNumberOfResults: number;
+}
