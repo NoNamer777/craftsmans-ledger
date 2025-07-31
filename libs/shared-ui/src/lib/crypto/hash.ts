b668ed78e8c8723ee32855fb132f37193b47c149
NoNamer777 <oscar.wellner@gmail.com>
2025-07-31 14:46:06 +0200

    Return hash in hex string format

--- libs/shared-ui/src/lib/crypto/hash.ts
+++ libs/shared-ui/src/lib/crypto/hash.ts
@@ -1,6 +1,11 @@
+/** Generates a SHA-512 hash from the given input and returns the hash in a hex string format. */
 export async function sha512(value: unknown) {
     const encoder = new TextEncoder();
     const data = encoder.encode(JSON.stringify(value));
 
-    return await window.crypto.subtle.digest('SHA-512', data);
+    const buffer = await window.crypto.subtle.digest('SHA-512', data);
+
+    return Array.from(new Uint8Array(buffer))
+        .map((byte) => byte.toString(16).padStart(2, '0'))
+        .join('');
 }
