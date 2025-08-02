01d520def573c285c1028e3e9617629244cb6e52
NoNamer777 <oscar.wellner@gmail.com>
2025-08-02 20:05:27 +0200

    Configure CORS

--- apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/core/AppRoutes.kt
+++ apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/core/AppRoutes.kt
@@ -1,13 +1,12 @@
 package org.eu.nl.craftsmansledger.core
 
 import io.ktor.http.*
-import io.ktor.serialization.JsonConvertException
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
@@ -15,12 +14,24 @@ import org.eu.nl.craftsmansledger.items.itemRoutes
 import org.eu.nl.craftsmansledger.technologyTrees.technologyTreeRoutes
 
 fun Application.appRoutes() {
+    val allowedDevOrigins = "https://localhost.admin.craftsmans-ledger.net:7100,https://localhost.www.craftsmans-ledger.net:7000,https://admin-dev.craftsmans-ledger.nl.eu.org,https://www-dev.craftsmans-ledger.nl.eu.org"
+
+    val ktorEnv = System.getenv("KTOR_ENV")
+    val allowedOrigins = System.getenv("CORS_ALLOWED_ORIGINS") ?: if (ktorEnv == "production") "" else allowedDevOrigins
+
     install(ContentNegotiation) {
         json(Json {
             isLenient = false
             prettyPrint = true
             allowStructuredMapKeys = false
         })
+    }
+    install(CORS) {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowOrigins { it -> if (allowedOrigins == "*") true else allowedOrigins.split(",").contains(it)  }
     }
 
     routing {
