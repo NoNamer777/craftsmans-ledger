b70f3fa6985da0d6489163ec8a688a9bfa6d04df
NoNamer777 <oscar.wellner@gmail.com>
2025-08-02 20:04:58 +0200

    Add ktor server cors dependency

--- apps/backend/build.gradle.kts
+++ apps/backend/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.host.common)
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.status.pages)
