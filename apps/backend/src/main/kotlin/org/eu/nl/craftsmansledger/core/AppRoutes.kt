dfab3ba8431813883e5834d81109af228599f2f4
NoNamer777 <oscar.wellner@gmail.com>
2025-08-02 11:37:54 +0200

    Add basic routing configuration

---
+++ apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/core/AppRoutes.kt
@@ -0,0 +1,42 @@
+package org.eu.nl.craftsmansledger.core
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
+
+fun Application.appRoutes() {
+    install(ContentNegotiation) {
+        json(Json {
+            isLenient = false
+            prettyPrint = true
+            allowStructuredMapKeys = false
+        })
+    }
+
+    routing {
+        staticResources("/assets", "assets")
+
+        install(StatusPages) {
+            exception<Throwable> { call, cause ->
+                val exception = InternalServerErrorException(cause.message ?: "An unexpected error occurred")
+
+                call.response.status(HttpStatusCode.fromValue(exception.status))
+                call.respond(exception)
+            }
+            exception<HttpException> { call, cause ->
+                call.response.status(HttpStatusCode.fromValue(cause.status))
+                call.respond(cause)
+            }
+        }
+
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
