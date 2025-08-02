0e57aed6c4066c67e72ad4ad074b9137b0778a9e
NoNamer777 <oscar.wellner@gmail.com>
2025-08-02 15:17:10 +0200

    Encapsulate item routes

---
+++ apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/items/ItemRoutes.kt
@@ -0,0 +1,71 @@
+package org.eu.nl.craftsmansledger.items
+
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receive
+import io.ktor.server.request.uri
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
+import io.ktor.server.routing.route
+import org.eu.nl.craftsmansledger.core.BadRequestException
+import org.eu.nl.craftsmansledger.core.NotFoundException
+
+fun Route.itemRoutes() {
+    route("/items") {
+        get {
+            call.respond(itemsService.getAll())
+        }
+
+        get("/query") {
+            val nameQuery = call.request.queryParameters["nameQuery"]
+
+            if (nameQuery == null) {
+                throw BadRequestException("Query parameter \"nameQuery\" is required")
+            }
+            call.respond(itemsService.query(nameQuery))
+        }
+
+        post {
+            val data = call.receive<CreateItemData>()
+            val url = call.request.uri
+
+            val item = itemsService.create(data)
+
+            call.response.headers.append(HttpHeaders.Location, "$url/${item.id}")
+            call.response.status(HttpStatusCode.Created)
+            call.respond(item)
+        }
+
+        route("/{itemId}") {
+            get {
+                val itemIdPathParam = call.parameters["itemId"]
+                val byId = itemsService.getById(itemId = itemIdPathParam!!)
+
+                if (byId == null) {
+                    throw NotFoundException("Item with ID \"$itemIdPathParam\" was not found")
+                }
+                call.respond(byId)
+            }
+
+            put {
+                val itemIdPathParam = call.parameters["itemId"]
+                val url = call.request.uri
+                val data = call.receive<Item>()
+
+                if (data.id != itemIdPathParam!!) {
+                    throw BadRequestException("It's not allowed to modify data of Item on path \"$url\" with data from Item with ID \"${data.id}\"")
+                }
+                call.respond(itemsService.update(data))
+            }
+
+            delete {
+                val itemIdPathParam = call.parameters["itemId"]
+                call.respond(itemsService.remove(itemIdPathParam!!))
+            }
+        }
+    }
+}
--- apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/model/ItemRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.eu.nl.craftsmansledger.model
-
-interface ItemRepository {
-    fun allItems(): List<Item>
-    fun itemByName(name: String): Item?
-    fun addItem(item: Item)
-    fun removeItem(name: String): Boolean
-}
