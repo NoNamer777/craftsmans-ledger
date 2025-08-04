18f1ac704378a80fd554b784e80030a6b91fff8f
NoNamer777 <oscar.wellner@gmail.com>
2025-08-04 17:38:16 +0200

    Extract common logic for resource type overviews

---
+++ apps/admin-portal/src/app/resources/overview/resource-types/base-resource-overview.component.ts
@@ -0,0 +1,83 @@
+import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NotificationService, notifyError, Resource } from '@craftsmans-ledger/shared-ui';
+import { catchError, filter, Observable, of, switchMap, tap } from 'rxjs';
+import { SaveAction } from '../../models';
+import { ActionsService } from '../actions.service';
+import { ResourceService } from '../resource.service';
+import { ResourceOption } from './components';
+
+@Component({
+    selector: 'cml-base-resource-overview',
+    template: '',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export abstract class BaseResourceOverviewComponent implements OnInit {
+    protected readonly destroyRef = inject(DestroyRef);
+    protected readonly resourceService = inject(ResourceService);
+    protected readonly actionsService = inject(ActionsService);
+    protected readonly notificationsService = inject(NotificationService);
+
+    protected readonly resourceOptions = signal<ResourceOption[]>([]);
+
+    protected readonly processing = signal(false);
+
+    protected readonly hasResourceSelected = computed(() => Boolean(this.resourceService.resourceId()));
+
+    public ngOnInit() {
+        this.getAllResources().pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+
+        this.actionsService.newResource$
+            .pipe(
+                tap(() => this.onNewResource()),
+                takeUntilDestroyed(this.destroyRef)
+            )
+            .subscribe();
+
+        this.actionsService.removeResource$
+            .pipe(
+                switchMap(() => this.onRemoveResource()),
+                catchError((error) => {
+                    notifyError(error, this.notificationsService);
+                    this.actionsService.removing.set(false);
+                    return of(null);
+                }),
+                filter(Boolean),
+                switchMap(() => {
+                    this.resourceService.resourceId.set(null);
+                    this.actionsService.reset();
+
+                    this.processing.set(true);
+                    return this.getAllResources();
+                }),
+                takeUntilDestroyed(this.destroyRef)
+            )
+            .subscribe();
+
+        this.actionsService.saveResource$
+            .pipe(
+                switchMap((action) => this.onSaveResource(action)),
+                switchMap((resource) => {
+                    this.actionsService.reset();
+                    this.resourceService.resourceId.set(resource.id);
+
+                    this.resourceService.resource.set(resource);
+                    this.resourceService.updatedResource.set(resource);
+
+                    this.processing.set(true);
+                    return this.getAllResources();
+                }),
+                takeUntilDestroyed(this.destroyRef)
+            )
+            .subscribe();
+    }
+
+    protected abstract getAllResources(): Observable<Resource[]>;
+
+    protected abstract onNewResource(): void;
+
+    protected abstract onSaveResource(action: SaveAction): Observable<Resource>;
+
+    protected abstract onRemoveResource(): Observable<boolean>;
+}
--- apps/admin-portal/src/app/resources/overview/resource-types/items/items-overview.component.ts
+++ apps/admin-portal/src/app/resources/overview/resource-types/items/items-overview.component.ts
@@ -1,21 +1,19 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
     CreateItemData,
     Item,
     ItemsService,
-    NotificationService,
     NotificationTypes,
     notifyError,
+    Resource,
     transform,
 } from '@craftsmans-ledger/shared-ui';
-import { catchError, filter, map, of, switchMap, tap } from 'rxjs';
-import { SaveActions, TEMP_RESOURCE_ID } from '../../../models';
-import { ActionsService } from '../../actions.service';
-import { ResourceService } from '../../resource.service';
-import { ResourceOption, ResourcesListComponent } from '../components';
+import { catchError, filter, map, of, tap } from 'rxjs';
+import { SaveAction, SaveActions, TEMP_RESOURCE_ID } from '../../../models';
+import { BaseResourceOverviewComponent } from '../base-resource-overview.component';
+import { ResourcesListComponent } from '../components';
 import { ItemForm } from './item.form';
 
 @Component({
@@ -25,130 +23,76 @@ import { ItemForm } from './item.form';
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [ResourcesListComponent, ItemForm, FormsModule],
 })
-export class ItemsOverviewComponent implements OnInit {
-    private readonly destroyRef = inject(DestroyRef);
-    private readonly resourceService = inject(ResourceService);
-    private readonly actionsService = inject(ActionsService);
+export class ItemsOverviewComponent extends BaseResourceOverviewComponent {
     private readonly itemsService = inject(ItemsService);
-    private readonly notificationsService = inject(NotificationService);
-
-    protected readonly itemOptions = signal<ResourceOption[]>([]);
-
-    protected readonly processing = signal(false);
-
-    protected readonly hasItemSelected = computed(() => Boolean(this.resourceService.resourceId()));
-
-    public ngOnInit() {
-        this.processing.set(true);
-
-        this.itemsService
-            .getAll()
-            .pipe(takeUntilDestroyed(this.destroyRef))
-            .subscribe({
-                next: (items) => this.setItemOptions(items),
-            });
-
-        this.actionsService.removeResource$
-            .pipe(
-                switchMap(() => {
-                    if (this.resourceService.resourceId() === TEMP_RESOURCE_ID) return of(null);
-                    return this.itemsService.remove(this.resourceService.resourceId()).pipe(map(() => true));
-                }),
-                catchError((error) => {
-                    notifyError(error, this.notificationsService);
-                    this.actionsService.removing.set(false);
-                    return of(null);
-                }),
-                filter(Boolean),
-                switchMap(() => {
-                    this.notificationsService.addNotification({
-                        type: NotificationTypes.SUCCESS,
-                        title: 'Item removed',
-                        message: `Item with ID "${this.resourceService.resourceId()}" was successfully removed.`,
-                    });
-
-                    this.resourceService.resourceId.set(null);
-                    this.actionsService.reset();
-
-                    this.processing.set(true);
-                    return this.itemsService.getAll();
-                }),
-                tap((items) => this.setItemOptions(items)),
-                takeUntilDestroyed(this.destroyRef)
-            )
-            .subscribe();
-
-        this.actionsService.newResource$
-            .pipe(
-                tap(() => {
-                    this.itemOptions.update((options) => [{ value: TEMP_RESOURCE_ID, label: 'New Item' }, ...options]);
-                    this.resourceService.resourceId.set(TEMP_RESOURCE_ID);
-                }),
-                takeUntilDestroyed(this.destroyRef)
-            )
-            .subscribe();
-
-        this.actionsService.saveResource$
-            .pipe(
-                switchMap((action) => {
-                    if (action === SaveActions.CREATE) {
-                        return this.itemsService
-                            .create(transform(CreateItemData, this.resourceService.updatedResource()))
-                            .pipe(
-                                catchError((error: HttpErrorResponse) => {
-                                    notifyError(error, this.notificationsService);
-                                    this.actionsService.saving.set(false);
-                                    return of(null);
-                                }),
-                                filter(Boolean),
-                                tap(({ id }) => {
-                                    this.notificationsService.addNotification({
-                                        type: NotificationTypes.SUCCESS,
-                                        title: 'Item created',
-                                        message: `Item with ID "${id}" was successfully created.`,
-                                    });
-                                })
-                            );
-                    }
-                    return this.itemsService.update(this.resourceService.updatedResource() as Item).pipe(
-                        catchError((error: HttpErrorResponse) => {
-                            notifyError(error, this.notificationsService);
-                            this.actionsService.saving.set(false);
-                            return of(null);
-                        }),
-                        filter(Boolean),
-                        tap(({ id }) => {
-                            this.notificationsService.addNotification({
-                                type: NotificationTypes.SUCCESS,
-                                title: 'Item updated',
-                                message: `Item with ID "${id}" was successfully updated.`,
-                            });
-                        })
-                    );
-                }),
-                switchMap((item) => {
-                    this.actionsService.reset();
-                    this.resourceService.resourceId.set(item.id);
-
-                    this.resourceService.resource.set(item);
-                    this.resourceService.updatedResource.set(item);
-
-                    this.processing.set(true);
-                    return this.itemsService.getAll();
-                }),
-                tap((items) => this.setItemOptions(items)),
-                takeUntilDestroyed(this.destroyRef)
-            )
-            .subscribe();
-    }
 
     protected onChangeSelectedResource() {
         if (this.resourceService.resourceId() !== TEMP_RESOURCE_ID) return;
-        this.itemOptions.update((options) => options.filter(({ value }) => value !== TEMP_RESOURCE_ID));
+        this.resourceOptions.update((options) => options.filter(({ value }) => value !== TEMP_RESOURCE_ID));
     }
 
-    private setItemOptions(items: Item[]) {
-        this.itemOptions.set(items.map(({ id, name }) => ({ label: name, value: id })));
+    protected override getAllResources() {
+        this.processing.set(true);
+
+        return this.itemsService.getAll().pipe(tap((items) => this.setResourceOptions(items)));
+    }
+
+    private setResourceOptions(items: Resource[]) {
+        this.resourceOptions.set(items.map(({ id, name }) => ({ label: name, value: id })));
         this.processing.set(false);
+    }
+
+    protected override onNewResource() {
+        this.resourceOptions.update((options) => [{ value: TEMP_RESOURCE_ID, label: 'New Item' }, ...options]);
+        this.resourceService.resourceId.set(TEMP_RESOURCE_ID);
+    }
+
+    protected override onSaveResource(action: SaveAction) {
+        if (action === SaveActions.CREATE) {
+            return this.itemsService.create(transform(CreateItemData, this.resourceService.updatedResource())).pipe(
+                catchError((error: HttpErrorResponse) => {
+                    notifyError(error, this.notificationsService);
+                    this.actionsService.saving.set(false);
+                    return of(null);
+                }),
+                filter(Boolean),
+                tap(({ id }) => {
+                    this.notificationsService.addNotification({
+                        type: NotificationTypes.SUCCESS,
+                        title: 'Item created',
+                        message: `Item with ID "${id}" was successfully created.`,
+                    });
+                })
+            );
+        }
+        return this.itemsService.update(this.resourceService.updatedResource() as Item).pipe(
+            catchError((error: HttpErrorResponse) => {
+                notifyError(error, this.notificationsService);
+                this.actionsService.saving.set(false);
+                return of(null);
+            }),
+            filter(Boolean),
+            tap(({ id }) => {
+                this.notificationsService.addNotification({
+                    type: NotificationTypes.SUCCESS,
+                    title: 'Item updated',
+                    message: `Item with ID "${id}" was successfully updated.`,
+                });
+            })
+        );
+    }
+
+    protected override onRemoveResource() {
+        if (this.resourceService.resourceId() === TEMP_RESOURCE_ID) return of(null);
+        return this.itemsService.remove(this.resourceService.resourceId()).pipe(
+            map(() => {
+                this.notificationsService.addNotification({
+                    type: NotificationTypes.SUCCESS,
+                    title: 'Item removed',
+                    message: `Item with ID "${this.resourceService.resourceId()}" was successfully removed.`,
+                });
+                return true;
+            })
+        );
     }
 }
