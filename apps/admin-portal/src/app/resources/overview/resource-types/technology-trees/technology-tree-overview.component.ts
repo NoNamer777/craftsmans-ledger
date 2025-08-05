9f15f971bcda6dff96720521c84aa206f6b1d320
NoNamer777 <oscar.wellner@gmail.com>
2025-08-05 14:34:36 +0200

    Remove returnType and fix service name

--- apps/admin-portal/src/app/resources/overview/resource-types/technology-trees/technology-tree-overview.component.ts
+++ apps/admin-portal/src/app/resources/overview/resource-types/technology-trees/technology-tree-overview.component.ts
@@ -4,16 +4,14 @@ import {
     CreateTechnologyTreeData,
     NotificationTypes,
     notifyError,
-    Resource,
     TechnologyTree,
     TechnologyTreesService,
     transform,
 } from '@craftsmans-ledger/shared-ui';
-import { catchError, filter, map, Observable, of, tap } from 'rxjs';
+import { catchError, filter, map, of, tap } from 'rxjs';
 import { SaveAction, SaveActions, TEMP_RESOURCE_ID } from '../../../models';
 import { BaseResourceOverviewComponent } from '../base-resource-overview.component';
 import { ResourcesListComponent } from '../components';
-import { ItemForm } from '../items/item.form';
 import { TechnologyTreeForm } from './technology-tree.form';
 
 @Component({
@@ -21,14 +19,14 @@ import { TechnologyTreeForm } from './technology-tree.form';
     templateUrl: './technology-tree-overview.component.html',
     styleUrl: './technology-tree-overview.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [ResourcesListComponent, ItemForm, TechnologyTreeForm],
+    imports: [ResourcesListComponent, TechnologyTreeForm],
 })
 export class TechnologyTreeOverviewComponent extends BaseResourceOverviewComponent {
-    private readonly technologyTreeService = inject(TechnologyTreesService);
+    private readonly technologyTreesService = inject(TechnologyTreesService);
 
     protected override getAllResources() {
         this.processing.set(true);
-        return this.technologyTreeService
+        return this.technologyTreesService
             .getAll()
             .pipe(tap((technologyTrees) => this.setResourceOptions(technologyTrees)));
     }
@@ -41,9 +39,9 @@ export class TechnologyTreeOverviewComponent extends BaseResourceOverviewCompone
         this.resourceService.resourceId.set(TEMP_RESOURCE_ID);
     }
 
-    protected override onSaveResource(action: SaveAction): Observable<Resource> {
+    protected override onSaveResource(action: SaveAction) {
         if (action === SaveActions.CREATE) {
-            return this.technologyTreeService
+            return this.technologyTreesService
                 .create(transform(CreateTechnologyTreeData, this.resourceService.updatedResource()))
                 .pipe(
                     catchError((error: HttpErrorResponse) => {
@@ -61,7 +59,7 @@ export class TechnologyTreeOverviewComponent extends BaseResourceOverviewCompone
                     })
                 );
         }
-        return this.technologyTreeService.update(this.resourceService.updatedResource() as TechnologyTree).pipe(
+        return this.technologyTreesService.update(this.resourceService.updatedResource() as TechnologyTree).pipe(
             catchError((error: HttpErrorResponse) => {
                 notifyError(error, this.notificationsService);
                 this.actionsService.saving.set(false);
@@ -80,7 +78,7 @@ export class TechnologyTreeOverviewComponent extends BaseResourceOverviewCompone
 
     protected override onRemoveResource() {
         if (this.resourceService.resourceId() === TEMP_RESOURCE_ID) return of(null);
-        return this.technologyTreeService.remove(this.resourceService.resourceId()).pipe(
+        return this.technologyTreesService.remove(this.resourceService.resourceId()).pipe(
             map(() => {
                 this.notificationsService.addNotification({
                     type: NotificationTypes.SUCCESS,
