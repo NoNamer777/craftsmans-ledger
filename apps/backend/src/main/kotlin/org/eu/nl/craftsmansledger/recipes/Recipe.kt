585a1a06136ea543cf74795f97993674acce95a3
NoNamer777 <oscar.wellner@gmail.com>
2025-08-07 21:49:14 +0200

    Add RecipeItem models and add inputs attribute to Recipe

--- apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/recipes/Recipe.kt
+++ apps/backend/src/main/kotlin/org/eu/nl/craftsmansledger/recipes/Recipe.kt
@@ -1,7 +1,20 @@
 package org.eu.nl.craftsmansledger.recipes
 
 import kotlinx.serialization.Serializable
+import org.eu.nl.craftsmansledger.items.Item
 import org.eu.nl.craftsmansledger.technologyTrees.TechnologyTree
+
+@Serializable
+data class RecipeItem(
+    val item: Item,
+    val quantity: Int,
+)
+
+@Serializable
+data class RecipeItemDto(
+    val itemId: String,
+    val quantity: Int,
+)
 
 @Serializable
 data class Recipe(
@@ -9,6 +22,17 @@ data class Recipe(
     var craftingTime: Double,
     var technologyTree: TechnologyTree,
     var technologyPoints: Int,
+    var inputs: List<RecipeItem> = mutableListOf(),
+) {
+    fun hasInputWithItem(itemId: String) = inputs.find { it.item.id == itemId } != null
+}
+
+@Serializable
+data class UpdateRecipeDto(
+    val id: String,
+    val craftingTime: Double,
+    val technologyTreeId: String,
+    val technologyPoints: Int,
 )
 
 data class CreateRecipeData(
@@ -19,14 +43,6 @@ data class CreateRecipeData(
 
 @Serializable
 data class CreateRecipeDto(
-    var craftingTime: Double,
-    var technologyTreeId: String,
-    var technologyPoints: Int,
-)
-
-@Serializable
-data class UpdateRecipeDto(
-    val id: String,
     var craftingTime: Double,
     var technologyTreeId: String,
     var technologyPoints: Int,
