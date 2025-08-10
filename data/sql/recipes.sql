7cbc6806f625ddbaa2d6d653ec0a1f064a396a50
NoNamer777 <oscar.wellner@gmail.com>
2025-08-10 14:42:35 +0200

    Add database dump

---
+++ data/sql/recipes.sql
@@ -0,0 +1,4 @@
+INSERT INTO cml_dev_db.recipes (id,crafting_time,technology_tree_id,technology_points) VALUES
+	 ('cTwPMKTvVyrYd8O2s16od',5.0,'BVSxVsBk8niTb_WCw5LRh',100),
+	 ('u1-ml2_5hTT_nY8JwvFv-',5.0,'BVSxVsBk8niTb_WCw5LRh',750),
+	 ('zDbglXgHjA9861xmxnnab',4.5,'BVSxVsBk8niTb_WCw5LRh',100);
---
+++ data/sql/technology-trees.sql
@@ -0,0 +1,5 @@
+INSERT INTO cml_dev_db.technology_trees (id,name,max_points) VALUES
+	 ('BVSxVsBk8niTb_WCw5LRh','Production',10000),
+	 ('pvv-vaPAdDDYc2KAFdKWi','Survival',5000),
+	 ('VN2T3J-vXIncnWfkO3rrx','Farming',10000),
+	 ('Zo02F_ZnYTW8XcO6o4YcW','Building',10000);
