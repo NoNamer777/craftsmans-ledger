5c309ffc4c1c90188fdfcad0b1588f5ada02261c
NoNamer777 <oscar.wellner@gmail.com>
2025-08-11 19:19:41 +0200

    CML-12: Reset stored content on key when it can't be parsed

--- libs/shared-ui/src/lib/browser-storage/browser-storage.service.ts
+++ libs/shared-ui/src/lib/browser-storage/browser-storage.service.ts
@@ -1,12 +1,19 @@
 import { inject, Injectable } from '@angular/core';
+import { tryCatch } from '../utils';
 import { BROWSER_STORAGE } from './providers';
 
 @Injectable({ providedIn: 'root' })
 export class BrowserStorageService {
     private readonly storage = inject(BROWSER_STORAGE);
 
-    public getItem(key: string) {
-        return JSON.parse(this.storage.getItem(key));
+    public async getItem(key: string) {
+        const { data, error } = await tryCatch(() => JSON.parse(this.storage.getItem(key)));
+
+        if (error) {
+            this.removeItem(key);
+            return null;
+        }
+        return data;
     }
 
     public setItem<T>(key: string, value: T) {
