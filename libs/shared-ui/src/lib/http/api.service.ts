35e175df9b4b162d00c2380e65d8c7f33f2922a2
NoNamer777 <oscar.wellner@gmail.com>
2025-08-11 19:21:22 +0200

    Get baseApiUrl whenever a request is made instead of upon initialization of ApiService

--- libs/shared-ui/src/lib/http/api.service.ts
+++ libs/shared-ui/src/lib/http/api.service.ts
@@ -8,24 +8,26 @@ export class ApiService {
     private readonly requestService = inject(RequestService);
     private readonly configService = inject(ConfigService);
 
-    public baseApiUrl = this.configService.config.baseApiUrl;
-
     public get<Response, QueryParamName extends string = string>(
         endPoint: string,
         queryParams?: QueryParams<QueryParamName>
     ) {
-        return this.requestService.get<Response>(`${this.baseApiUrl}${endPoint}`, queryParams);
+        const baseApiUrl = this.configService.config.baseApiUrl;
+        return this.requestService.get<Response>(`${baseApiUrl}${endPoint}`, queryParams);
     }
 
     public post<RequestBody, Response>(endpoint: string, body: RequestBody) {
-        return this.requestService.post<RequestBody, Response>(`${this.baseApiUrl}${endpoint}`, body);
+        const baseApiUrl = this.configService.config.baseApiUrl;
+        return this.requestService.post<RequestBody, Response>(`${baseApiUrl}${endpoint}`, body);
     }
 
     public put<RequestBody, Response = RequestBody>(endpoint: string, body: RequestBody) {
-        return this.requestService.put<RequestBody, Response>(`${this.baseApiUrl}${endpoint}`, body);
+        const baseApiUrl = this.configService.config.baseApiUrl;
+        return this.requestService.put<RequestBody, Response>(`${baseApiUrl}${endpoint}`, body);
     }
 
     public delete(endPoint: string) {
-        return this.requestService.delete(`${this.baseApiUrl}${endPoint}`);
+        const baseApiUrl = this.configService.config.baseApiUrl;
+        return this.requestService.delete(`${baseApiUrl}${endPoint}`);
     }
 }
