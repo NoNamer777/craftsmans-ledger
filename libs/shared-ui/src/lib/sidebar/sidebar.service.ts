feefc745e66ca96d940f16e79fbccf9c36427fc5
NoNamer777 <oscar.wellner@gmail.com>
2025-08-13 18:17:23 +0200

    CML-8: Properly type the sidebar close result

--- libs/shared-ui/src/lib/sidebar/sidebar.service.ts
+++ libs/shared-ui/src/lib/sidebar/sidebar.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal, Type } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class SidebarService {
+export class SidebarService<T = unknown> {
     public readonly component = signal<Type<unknown>>(null);
 
     public readonly title = signal('Sidebar');
@@ -10,14 +10,14 @@ export class SidebarService {
     private readonly openSubject = new Subject<void>();
     public readonly open$ = this.openSubject.asObservable();
 
-    private readonly closeSubject = new Subject<unknown>();
+    private readonly closeSubject = new Subject<T>();
     public readonly close$ = this.closeSubject.asObservable();
 
     public openSidebar() {
         this.openSubject.next();
     }
 
-    public closeSidebar<T>(result: T) {
+    public closeSidebar(result: T) {
         this.closeSubject.next(result);
     }
 }
