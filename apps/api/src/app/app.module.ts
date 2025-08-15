70be9d3dec101c77a1ef170134f3ba823920810d
NoNamer777 <oscar.wellner@gmail.com>
2025-08-16 00:18:18 +0200

    Scout: Establish and break database connection upon app start up and teardown

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { configModuleOptions } from './core';
+import { configModuleOptions, DatabaseModule } from './core';
 
 @Module({
-    imports: [ConfigModule.forRoot(configModuleOptions)],
+    imports: [ConfigModule.forRoot(configModuleOptions), DatabaseModule],
     controllers: [AppController],
 })
 export class AppModule {}
--- apps/api/src/app/core/index.ts
+++ apps/api/src/app/core/index.ts
@@ -1,1 +1,2 @@
 export * from './config/constants';
+export * from './database';
