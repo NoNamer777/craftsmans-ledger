5992c9ae624660be5c96aad2e02bb67c01949d7b
NoNamer777 <oscar.wellner@gmail.com>
2025-08-16 00:25:23 +0200

    Scout: Fix imports

--- apps/admin-portal/src/app/resources/overview/resource-types/items/items-overview.component.ts
+++ apps/admin-portal/src/app/resources/overview/resource-types/items/items-overview.component.ts
@@ -1,14 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {
-    CreateItemData,
-    Item,
-    ItemsService,
-    NotificationTypes,
-    notifyError,
-    transform,
-} from '@craftsmans-ledger/shared-ui';
+import { transform } from '@craftsmans-ledger/shared';
+import { CreateItemData, Item, ItemsService, NotificationTypes, notifyError } from '@craftsmans-ledger/shared-ui';
 import { catchError, filter, map, of, tap } from 'rxjs';
 import { SaveAction, SaveActions, TEMP_RESOURCE_ID } from '../../../models';
 import { BaseResourceOverviewComponent } from '../base-resource-overview.component';
