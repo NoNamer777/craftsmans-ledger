94176f57cc463737326f5111f0c81dd3c96a7bbe
NoNamer777 <oscar.wellner@gmail.com>
2025-08-18 17:22:24 +0200

    Scout: Export standard query params model

--- libs/shared/src/lib/http/index.ts
+++ libs/shared/src/lib/http/index.ts
@@ -2,3 +2,4 @@ export * from './paginated-response';
 export * from './query-params';
 export * from './recipe-query-params';
 export * from './sort-order';
+export * from './standard-query-params';
