69b63117524c4f74e7bef34befc72d71df58c805
NoNamer777 <oscar.wellner@gmail.com>
2025-08-18 17:24:25 +0200

    Scout: Add ResourceTypes models

--- libs/shared/src/lib/resources/index.ts
+++ libs/shared/src/lib/resources/index.ts
@@ -1,5 +1,6 @@
 export * from './comparable.model';
 export * from './items';
 export * from './recipes';
+export * from './resource-type.model';
 export * from './resource.model';
 export * from './technology-trees';
