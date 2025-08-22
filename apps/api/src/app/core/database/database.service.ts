0bdcefee8082f2ae9bdd2c92ab3690843bf502a4
NoNamer777 <oscar.wellner@gmail.com>
2025-08-22 11:49:35 +0200

    Refactor how Prisma client is used and from which path

--- apps/api/src/app/core/database/database.service.ts
+++ apps/api/src/app/core/database/database.service.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@craftsmans-ledger/prisma';
+import { tryCatch } from '@craftsmans-ledger/shared';
 import { BeforeApplicationShutdown, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnApplicationBootstrap, BeforeApplicationShutdown {
+export class DatabaseService implements OnApplicationBootstrap, BeforeApplicationShutdown {
+    public readonly prismaClient = new PrismaClient();
+
     public async onApplicationBootstrap() {
-        await this.$connect();
+        const { error } = await tryCatch(this.prismaClient.$connect());
+
+        if (error) console.error(error);
     }
 
     public async beforeApplicationShutdown() {
-        await this.$disconnect();
+        await this.prismaClient.$disconnect();
     }
 }
